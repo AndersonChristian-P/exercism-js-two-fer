62b7df8ede75cd3c07d9249a4eb7926ea7a09fd2
Christian Anderson <ca96187@gmail.com>
2019-08-18 16:14:04 -0600

    used DRY principle and default parameter to refactor code

--- two-fer.js
+++ two-fer.js
@@ -1,7 +1,26 @@
-export const twoFer = (name) => {
+/* export const twoFer = (name) => {
   if (name === undefined) {
     return `One for you, one for me.`
   } else {
     return `One for ${name}, one for me.`
   }
+}
+
+this doesn't follow the DRY principle because you have 2 return statements
+
+*/
+
+/* export const twoFer = (name) => {
+  return (name ? `One for ${name}, one for me` : 'One for you, one for me.')
+}
+
+this can be simplified to the following 
+
+export const twoFer = (name) => {
+  return (`One for ${name || "you"}, one for me.`)
+}*/
+
+
+export const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`
 }
